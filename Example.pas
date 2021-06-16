59420cb1394102a9866f3947bc2a66eb05875e72
1Jae <wjkim@linkhub.co.kr>
2021-06-16 11:58:33 +0900

    added UseLocalTimeYN Config

--- Example.pas
+++ Example.pas
@@ -119,6 +119,9 @@ begin
 
         //인증토큰 IP제한기능 사용여부, true(권장)
         closedownService.IPRestrictOnOff := true;
+
+        //로컬시스템 시간 사용여부, true(사용), false(미사용) - 기본값
+        closedownService.UseLocalTimeYN := false;
 end;
 
 function BoolToStr(b:Boolean):String;
